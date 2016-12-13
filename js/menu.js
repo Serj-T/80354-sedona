ef4a8213ee0faea93d78da081c082387436e2ef8
Serj T <serjtelepov@gmail.com>
2016-12-13 13:00:21 +0300

    +3 page adaptive, issue with form wraps, +js menu

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+var navMain = document.querySelector('.main-nav');
+var navBtnOpen = document.querySelector('.main-nav__btn-open');
+var navBtnClose = document.querySelector('.main-nav__btn-close');
+
+navMain.classList.remove('main-nav--nojs');
+
+navBtnOpen.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+navBtnClose.addEventListener('click', function() {
+  navMain.classList.remove('main-nav--opened');
+  navMain.classList.add('main-nav--closed');
+});
